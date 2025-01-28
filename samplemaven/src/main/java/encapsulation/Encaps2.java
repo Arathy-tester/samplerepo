8869c9f440e73e90777bfecce717741cd51c9316
prade <prade@prvlenovo>
2025-01-28 19:28:48 +0530

    Encapsulation private variable

---
+++ samplemaven/src/main/java/encapsulation/Encaps2.java
@@ -0,0 +1,14 @@
+package encapsulation;
+
+public class Encaps2 {
+
+	public static void main(String[] args) {
+		Encap1 obj=new Encap1();
+		obj.setter("Ammu",22);
+		obj.getter();
+	}
+		// TODO Auto-generated method stub
+
+	}
+
+
